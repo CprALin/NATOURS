b24e1cb9765fa14343884a8e91527718335ee2e4
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2023-12-24 12:55:01 +0200

    -

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,7 +36,63 @@ const Tour = require('./../models/tourModel');
     //console.log(req.requestTime);
   try 
   {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    //Filtering
+    const queryObj = {...req.query};
+    const excludeFields = ['page' , 'sort' , 'limit' , 'fields'];
+
+    excludeFields.forEach(el => delete queryObj[el]);
+     
+    //console.log(req.query , queryObj);
+    
+    //Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    // gte , gt , lte , lt
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    //console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //{ difficulty : 'easy' , duration : { $gte : 5 }}
+    
+    //Sorting
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(',').join(' ');
+        //console.log(sortBy);
+        query = query.sort(sortBy);
+        // sort('price ratingAvarage')
+    }else{
+        query = query.sort('-createdAt');
+    }
+
+    //Field limiting
+    if(req.query.fields){
+        const fields = req.query.fields.split(',').join(' ');
+        query = query.select(fields);
+    }else{
+        query = query.select('-__v');
+    }
+
+    //Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10 , 1-10 page 1 , 11-20 page 2 , 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if(req.query.page){
+        const numTours = await Tour.countDocuments();
+        if(skip >= numTours)
+        {
+            throw new Error('This page does not exits');
+        }
+    }
+
+    //EXECUTE QUERY
+    const tours = await query; 
+    //query.sort().select().skip().limit()...
 
     res.status(200).json({
         status : 'success',
