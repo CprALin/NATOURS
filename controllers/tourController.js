0eebca4f99d34623e25642bbc488625851a19721
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2023-12-26 14:55:35 +0200

    -

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -147,7 +147,12 @@ exports.updateTour = catchAsync(async (req , res , next) => {
 
 exports.deleteTour = catchAsync(async (req , res) => {
     
-  await Tour.findByIdAndDelete(req.params.id); 
+  const tour = await Tour.findByIdAndDelete(req.params.id); 
+
+  if(!tour)
+  {
+    return next(new AppError('No tour found with that ID', 404));
+  }
 
   res.status(204).json({
       status : 'success',
