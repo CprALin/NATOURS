8036de994a23d4852b14aac464c447bcff75ad2c
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-01-26 13:43:08 +0200

    -

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,25 +8,26 @@ const router = express.Router();
 
 router
     .post('/singup' , authController.singup);
-
 router
     .post('/login' , authController.login);
-
 router
     .post('/forgotPassword' , authController.forgotPassword);
-    
 router
     .patch('/resetPassword/:token' , authController.resetPassword);    
 
-router
-    .patch('/updateMyPassword' , authController.protect , authController.updatePassword);
-router    
-    .get('/me' , authController.protect , userController.getMe , userController.getUser);
-router 
-    .patch('/updateMe' , authController.protect , userController.updateMe);  
+router.use(authController.protect);    
 
 router
-    .delete('/deleteMe' , authController.protect , userController.deleteMe);    
+    .patch('/updateMyPassword' , authController.updatePassword);
+router    
+    .get('/me' , userController.getMe , userController.getUser);
+router 
+    .patch('/updateMe' , userController.updateMe);  
+
+router
+    .delete('/deleteMe' , userController.deleteMe);    
+
+router.use(authController.restrictTo('admin'));
 
 router 
     .route('/')
