0637e1b54586e06b49bdb57c87a411b4f1356dd9
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-01-27 20:58:08 +0200

    -

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -17,8 +17,18 @@ exports.getOverview = catchAsync(async (req , res) => {
     });
 });
 
-exports.getTour = (req , res) => {
-    res.status(200).render('tour' , {
-        title : 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req , res) => {
+    //get data from collection
+    const tour = await Tour.findOne({slug : req.params.slug}).populate({
+        path : 'reviews',
+        fields : 'review rating user'
     });
-}
+    //build  tamplate
+
+    //render using tour data
+
+    res.status(200).render('tour' , {
+        title : 'The Forest Hiker Tour',
+        tour
+    });
+});
