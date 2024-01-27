e8d7ebe7622d4f1a962b3a08e2ebeb1b0cdf2367
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-01-27 18:35:06 +0200

    -

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,21 @@
-exports.getOverview = (req , res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req , res) => {
+
+    // get data from collection
+    const tours = await Tour.find();
+
+
+    //build tamplate 
+
+    //render tamplate using tour data
+
     res.status(200).render('overview' , {
-        title : 'All Tours'
+        title : 'All Tours',
+        tours
     });
-};
+});
 
 exports.getTour = (req , res) => {
     res.status(200).render('tour' , {
